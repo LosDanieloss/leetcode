2d8844b70cc27ecd63254353405dbd5ed677bf5c
Daniel <danielojewski@gmail.com>
2025-01-08 22:50:11 +0100

    feat[101]: Symmetric Tree

--- easy/src/main/java/com/jeglikowski/easy/e0101/SymmetricTree.kt
+++ easy/src/main/java/com/jeglikowski/easy/e0101/SymmetricTree.kt
@@ -1,5 +1,7 @@
 package com.jeglikowski.easy.e0101
 
+import java.util.Stack
+
 /**
  * Example:
  * var ti = TreeNode(5)
@@ -17,12 +19,90 @@ private class TreeNode(var `val`: Int) {
 }
 
 private class Solution {
-
-    val leftSide = mutableListOf<Int?>()
-    val rightSide = mutableListOf<Int?>()
-
     fun isSymmetric(root: TreeNode?): Boolean {
+        return iterative(root)
+    }
+
+    fun iterative(root: TreeNode?): Boolean {
+        if (root == null) {
+            return true
+        }
+        val crossRoadsQueue = Stack<TreeNode?>().apply {
+            add(root.left)
+            add(root.right)
+        }
+        while (crossRoadsQueue.isNotEmpty()) {
+            val p = crossRoadsQueue.pop()
+            val q = crossRoadsQueue.pop()
+            if (p == null && q == null) {
+                continue
+            }
+            if (p == null) {
+                return false
+            }
+            if (q == null) {
+                return false
+            }
+            if (p.`val` != q.`val`) {
+                return false
+            }
+            crossRoadsQueue.add(p.left)
+            crossRoadsQueue.add(q.right)
+            crossRoadsQueue.add(p.right)
+            crossRoadsQueue.add(q.left)
+        }
+
         return true
+    }
+
+    fun iterativePairs(root: TreeNode?): Boolean {
+        if (root == null) {
+            return true
+        }
+        var p = root.left
+        var q = root.right
+        val crossRoadsQueue = Stack<Pair<TreeNode?, TreeNode?>>().apply {
+            add(root.left to root.right)
+        }
+        while (crossRoadsQueue.isNotEmpty()) {
+            val nextPair = crossRoadsQueue.pop()
+            p = nextPair?.first
+            q = nextPair?.second
+            if (p == null && q == null) {
+                continue
+            }
+            if (p == null) {
+                return false
+            }
+            if (q == null) {
+                return false
+            }
+            if (p.`val` != q.`val`) {
+                return false
+            }
+            crossRoadsQueue.add(p.left to q.right)
+            crossRoadsQueue.add(p.right to q.left)
+        }
+
+        return p?.`val` == q?.`val`
+    }
+
+    fun recursive(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) {
+            return true
+        }
+        if (p == null) {
+            return false
+        }
+        if (q == null) {
+            return false
+        }
+        if (p.`val` != q.`val`) {
+            return false
+        }
+        val isInnerSame = recursive(p.right, q.left)
+        val isOuterSame = recursive(p.left, q.right)
+        return isInnerSame && isOuterSame
     }
 }
 
@@ -38,8 +118,6 @@ fun main() {
     val result = solution.isSymmetric(root)
     println(result) // true
 
-    solution.leftSide.clear()
-    solution.rightSide.clear()
     val root2 = TreeNode(1)
     root2.left = TreeNode(2)
     root2.right = TreeNode(2)
@@ -48,8 +126,6 @@ fun main() {
     val result2 = solution.isSymmetric(root2)
     println(result2) // false
 
-    solution.leftSide.clear()
-    solution.rightSide.clear()
     val root3 = TreeNode(1)
     root3.left = TreeNode(2)
     root3.right = TreeNode(2)
@@ -59,5 +135,40 @@ fun main() {
     root3.right?.right = null
     val result3 = solution.isSymmetric(root3)
     println(result3) // false
+
+    val root4 = TreeNode(1)
+    val result4 = solution.isSymmetric(root4)
+    println(result4) // true
+
+    val root5 = TreeNode(1).apply {
+        left = TreeNode(2)
+        right = TreeNode(2)
+    }
+    val result5 = solution.isSymmetric(root5)
+    println(result5) // true
+
+    val root6 = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            left = TreeNode(1)
+        }
+        right = TreeNode(2)
+    }
+    val result6 = solution.isSymmetric(root6)
+    println(result6) // false
+
+    val root7 = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            left = TreeNode(1).apply {
+                left = TreeNode(2)
+            }
+        }
+        right = TreeNode(2).apply {
+            right = TreeNode(1).apply {
+                right = TreeNode(2)
+            }
+        }
+    }
+    val result7 = solution.isSymmetric(root7)
+    println(result7) // false
 
 }
